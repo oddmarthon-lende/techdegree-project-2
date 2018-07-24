a5d7565bc84f3e30b938381a7dc6cc813cf393df
Odd Marthon Lende <oddmarthon.lende@me.com>
2018-07-24 19:35:43 +0200

    Added closure

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 
-const $h1 = $('<h1></h1>');
+(function() {
 
-pagination.init(".page", 10)
-          .new("ul.student-list");
+  const $h1 = $('<h1></h1>');
 
-search.init(".page-header", "student-search", "Search for students")
-      .new("ul.student-list", (results, text) => {
+  pagination.init(".page", 10)
+            .new("ul.student-list");
 
-        if(!results.length) {
-          $h1.text(`No results when searching for "${text}".`)
-          $('.page').append($h1);
-        }
-        else {
-          $h1.remove();
-        }
+  search.init(".page-header", "student-search", "Search for students")
+        .new("ul.student-list", (results, text) => {
 
-        pagination.new(results);
+          if(!results.length) {
+            $h1.text(`No results when searching for "${text}".`)
+            $('.page').append($h1);
+          }
+          else {
+            $h1.remove();
+          }
+
+          pagination.new(results);
+
+        });
         
-      });
+})();
