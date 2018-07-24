768f5492fdf12e646e3e22bab3607b7551f2fd81
Odd Marthon Lende <oddmarthon.lende@me.com>
2018-07-24 18:40:53 +0200

    Finished Project

---
+++ js/pagination.js
@@ -0,0 +1,85 @@
+const pagination = {
+
+  _initialized: false,
+
+  init: function(paginationLinksContainer, elementsPerPage) {
+
+    const $div = $('<div class="pagination"></div>');
+    const $ul  = $('<ul></ul>');
+    const self = this;
+
+    if(this._initialized)
+      throw new Error("Already initialized");
+
+    $div.append($ul);
+    $(paginationLinksContainer).append($div);
+
+    this._$links = $ul;
+    this._initialized = true;
+    this._perPage = elementsPerPage || 10;
+
+    $ul.on('click', 'a', function(e) {
+      e.preventDefault();
+      self.page(parseInt($(this).text()));
+    });
+
+    return this;
+  },
+
+  new: function(container) {
+
+    let elements,
+        $links = this._$links;
+
+    if(!this._initialized)
+      throw new Error("Pagination not initialized");
+
+    this._elements =
+          elements = Array.isArray(container) ?
+                                      container : Array.from($(container)[0].children);
+
+    $links.html('');
+
+    if(!elements.length)
+      return this;
+
+    for(var i = 0; i < Math.ceil(elements.length / this._perPage); i++) {
+
+      const $link = $(`
+        <li>
+          <a href="#">${i + 1}</a>
+        </li>
+      `);
+
+      $links.append($link);
+
+    }
+
+    this.page(1);
+
+    return this;
+  },
+
+  page: function(page) {
+
+    const elements = this._elements;
+    const perPage = this._perPage;
+    const $links  = this._$links.find('a');
+    const start = (page - 1) * perPage;
+    const end   = (start + perPage);
+    const range = elements.slice(start, end);
+
+    $(elements).hide();
+    $(range).show();
+
+    for(let link of $links) {
+      if(parseInt($(link).text()) === page)
+        $(link).addClass('active')
+      else {
+        $(link).removeClass('active')
+      }
+    }
+
+    return this;
+  }
+};
---
+++ js/search.js
@@ -0,0 +1,65 @@
+const search = {
+
+  _initialized: false,
+
+  init: function(searchFieldContainer, classNames, placeholder) {
+
+    const $div = $(`<div class="${classNames}"></div>`)
+    const $input = $(`<input placeholder="${placeholder}">`);
+    const $button = $(`<button>Search</button>`);
+
+    const invoke = () => this.find($input.val());
+
+    if(this._initialized)
+      throw new Error("Already initialized");
+
+    $div.append($input)
+        .append($button);
+
+    $input.on('keyup', invoke);
+    $button.on('click', invoke);
+
+    $(searchFieldContainer).append($div);
+
+    this._initialized = true;
+
+    return this;
+  },
+
+  new: function(container, callback) {
+
+    if(!this._initialized)
+      throw new Error("Search not initialized");
+
+    this._elements =
+          elements = Array.isArray(container) ?
+                                      container : Array.from($(container)[0].children);
+    this._callback = callback;
+
+  },
+
+  find: function(text) {
+
+    const elements = this._elements;
+    const callback = this._callback;
+    const results = [];
+
+    if(!this._initialized)
+      throw new Error("Search not initialized");
+
+    for(let element of elements) {
+
+      if($(element).text().indexOf(text) > -1)
+        results.push(element);
+    }
+
+    $(elements).hide();
+    $(results).show();
+
+    if(typeof callback === 'function')
+      callback(results);
+
+    return this;
+  }
+
+};
